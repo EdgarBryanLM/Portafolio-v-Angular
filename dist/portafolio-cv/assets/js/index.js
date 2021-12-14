cc2a48b741fce4110539ca4ec147aa641fc3e859
Edgar <s17120227@alumnos.itsur.edu.mx>
2021-12-13 18:31:26 -0600

    cambios

---
+++ dist/portafolio-cv/assets/js/index.js
@@ -0,0 +1,107 @@
+window.sr=ScrollReveal();
+
+
+sr.reveal('.alinear',{
+   duration:3000,
+    origin:'bottom',
+    distance: '-100px'
+});
+
+
+sr.reveal('.tblanco',{
+   duration:3000,
+    origin:'left',
+    distance: '200px'
+});
+sr.reveal('.men2',{
+   duration:3000,
+    origin:'right',
+    distance: '200px'
+});
+
+sr.reveal('.col-a',{
+   duration:2000,
+    origin:'left',
+    distance: '100px'
+    
+});
+sr.reveal('.col-a2',{
+   duration:1000,
+    origin:'bottom',
+    distance: '200px'
+    
+});
+
+sr.reveal('.col-a3',{
+   duration:3000,
+    origin:'bottom',
+    distance: '200px'
+    
+});
+
+sr.reveal('.col-a4',{
+   duration:4000,
+    origin:'bottom',
+    distance: '200px'
+    
+});
+
+sr.reveal('.col-a5',{
+   duration:4500,
+    origin:'bottom',
+    distance: '200px'
+    
+});
+
+sr.reveal('.redes',{
+   duration:2500,
+    origin:'bottom',
+    distance: '200px'
+    
+});
+sr.reveal('.contenido-form',{
+   duration:2500,
+    origin:'left',
+    distance: '100px'
+    
+});
+sr.reveal('.copi',{
+   duration:1500,
+    origin:'bottom',
+    distance: '100px'
+    
+});
+
+sr.reveal('.text-footer',{
+   duration:1500,
+    origin:'left',
+    distance: '100px'
+    
+});
+
+sr.reveal('.tec-mov',{
+   duration:1500,
+    origin:'bottom',
+    distance: '100px'
+    
+});
+
+sr.reveal('.gto',{
+   duration:1500,
+    origin:'left',
+    distance: '100px'
+    
+});
+sr.reveal('.btoo',{
+   duration:1500,
+    origin:'bottom',
+    distance: '100px'
+    
+});
+
+sr.reveal('.grupo',{
+   duration:1500,
+    origin:'bottom',
+    distance: '100px'
+    
+});
